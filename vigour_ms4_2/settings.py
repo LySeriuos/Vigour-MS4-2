2db0970ed4371c5e882d5b57ae3cb2cfe71cb3df
LySeriuos <drungilas.p@gmail.com>
2022-03-26 19:56:14 +0000

    added home app and templates

--- vigour_ms4_2/settings.py
+++ vigour_ms4_2/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount'
+    'allauth.socialaccount',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,10 @@ ROOT_URLCONF = 'vigour_ms4_2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
