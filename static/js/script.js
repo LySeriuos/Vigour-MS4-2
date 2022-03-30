6087535a56150402bd07fa76ae8631dd150dbe1f
LySeriuos <drungilas.p@gmail.com>
2022-03-30 21:29:28 +0000

    fixed tiny-slider for the community section

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
-    let slider = tns({
-        container : ".my-slider",
-        "slideBy" : "1",
+const slider1 = tns({
+    container: ".slider1",
+    "slideBy" : "1",
         "speed" : 400,
         "nav" : false,
         autoplay : true,
@@ -24,6 +24,34 @@
                 gutter : 20
             }
         }
-    }); 
+});
+
+const slider2 = tns({
+    container: ".slider2",
+    "slideBy" : "1",
+        "speed" : 400,
+        "nav" : false,
+        autoplay : true,
+        // controls: false,
+        autoplayButtonOutput : false,
+        responsive: {
+            1600: {
+                items : 4,
+                gutter : 20
+            },
+            1024: {
+                items : 3,
+                gutter : 20
+            },
+            768: {
+                items : 2,
+                gutter : 20
+            },
+            480: {
+                items : 1,
+                gutter : 20
+            }
+        }
+});
 
    
