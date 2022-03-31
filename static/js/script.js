84ec99e102777e151ea1e2575fe84da3732d40a9
LySeriuos <drungilas.p@gmail.com>
2022-03-31 15:05:24 +0000

    added images to community section

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,14 @@
-const slider1 = tns({
+let slider1 = tns({
     container: ".slider1",
     "slideBy" : "1",
         "speed" : 400,
         "nav" : false,
         autoplay : true,
         // controls: false,
+        controlsPosition: "bottom",
         autoplayButtonOutput : false,
+        prevButton: '.prev',
+        nextButton: '.next',
         responsive: {
             1600: {
                 items : 4,
@@ -26,14 +29,17 @@ const slider1 = tns({
         }
 });
 
-const slider2 = tns({
+let slider2 = tns({
     container: ".slider2",
     "slideBy" : "1",
         "speed" : 400,
         "nav" : false,
         autoplay : true,
         // controls: false,
+        controlsPosition: "bottom",
         autoplayButtonOutput : false,
+        prevButton: '.prev-com',
+        nextButton: '.next-com',
         responsive: {
             1600: {
                 items : 4,
