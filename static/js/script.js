b5b272e340032c2a9fed78dd3415184c95bb474b
LySeriuos <drungilas.p@gmail.com>
2022-04-02 16:20:38 +0000

    changed dropddown icons to arrow-down, this suits better for mobile nav

--- static/js/script.js
+++ static/js/script.js
@@ -59,5 +59,19 @@ let slider2 = tns({
             }
         }
 });
-
-   
+$(document).on('click', '.dropdown-menu', function (e) {
+    e.stopPropagation();
+  });
+  
+  // make it as accordion for smaller screens
+  if ($(window).width() < 992) {
+    $('.dropdown-menu a').click(function(e){
+      e.preventDefault();
+        if($(this).next('.submenu').length){
+          $(this).next('.submenu').toggle();
+        }
+        $('.dropdown').on('hide.bs.dropdown', function () {
+       $(this).find('.submenu').hide();
+    })
+    });
+  }
