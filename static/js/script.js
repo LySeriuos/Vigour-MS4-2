f5a48c71df6245bbf8ecb0cbb8ae5faa9e15894c
LySeriuos <drungilas.p@gmail.com>
2022-04-03 11:30:30 +0000

    changed meal section to tiny slider for mobile phones, it takes much less space for users

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// Tiny slider JS for training programs
 let slider1 = tns({
     container: ".slider1",
     "slideBy" : "1",
@@ -29,6 +30,7 @@ let slider1 = tns({
         }
 });
 
+// Tiny slider JS for Meal section
 let slider3 = tns({
     container: ".slider3",
     "slideBy" : "1",
@@ -41,22 +43,52 @@ let slider3 = tns({
         prevButton: '.prev-meal',
         nextButton: '.next-meal',
         responsive: {
-            1600: {
+            1024: {
                 items : 3,
                 gutter : 20
             },
-            800: {
+            768: {
                 items : 2,
                 gutter : 20
             },
-            768: {
+            480: {
                 items : 1,
                 gutter : 20
             }
         }
+          
+        
 });
 
+// Tiny slider JS for Meal section
+let slider4 = tns({
+    container: ".slider4",
+    "slideBy" : "1",
+        // "speed" : 400,
+        "nav" : false,
+        autoplay : false,
+        // controls: true,
+        controlsPosition: "bottom",
+        autoplayButtonOutput : false,
+        prevButton: '.prev-price',
+        nextButton: '.next-price',
+        responsive: {
+            1024: {
+                items : 3,
+                gutter : 20
+            },
+            768: {
+                items : 2,
+                gutter : 20
+            },
+            480: {
+                items : 1,
+                gutter : 20
+            }
+        }       
+});
 
+// Tiny slider JS for Community
 let slider2 = tns({
     container: ".slider2",
     "slideBy" : "1",
@@ -87,6 +119,8 @@ let slider2 = tns({
             }
         }
 });
+
+// Dropdown multi level for main nav
 $(document).on('click', '.dropdown-menu', function (e) {
     e.stopPropagation();
   });
