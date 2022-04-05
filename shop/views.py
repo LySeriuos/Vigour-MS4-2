0b24250b3b7dee9ffacc4b64576b2cc6272d1ad8
LySeriuos <drungilas.p@gmail.com>
2022-04-05 14:28:29 +0000

    added shop views and templates

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 
 def shop(request):
     """A view to return the shop page"""
-    return render(request, 'shop/shop.html')
+    products = Product.objects.all()
+
+    context = {
+        'shop': products,
+    }
+
+    return render(request, 'shop/shop.html', context)
+
+# copied from home/views.py.
+
+  
