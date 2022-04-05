088e88309b9b072da885e1479cfc273c249a3e6f
LySeriuos <drungilas.p@gmail.com>
2022-04-05 11:31:44 +0000

    loaded fixtures data

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+
+# use this to load data python3 loaddata categories
+# start with categories since the products
+# need to know which category to go in.
--- products/models.py
+++ products/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    subcategory = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -14,12 +15,16 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+    def get_subcategory(self):
+        return self.subcategory
+
 
 # null=True, blank=True this gives us option to use products which do not have
 # some of the categories or the values is empty
 
 # on_delete=models.SET_NULL it gives option to make empty that category
 # in database rather than deleting product.
+
 
 class Product(models.Model):
     category = models.ForeignKey(
@@ -32,10 +37,18 @@ class Product(models.Model):
     list2 = models.CharField(max_length=254, null=True, blank=True)
     list3 = models.CharField(max_length=254, null=True, blank=True)
     list4 = models.CharField(max_length=254, null=True, blank=True)
+    daily_dose = models.CharField(max_length=254, null=True, blank=True)
+    old_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     subcategory = models.CharField(max_length=254, null=True, blank=True)
-    
+
+    def __str__(self):
+        return self.name
+
+# use 'python3 manage.py makemigrations --dry-run'
+# to find if there are any issues in the project before making changes
+# use python3 manage.py migrate --plan to see which migrations going to be
