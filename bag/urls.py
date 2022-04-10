bec4703d072bc37d26f7c08be5cdd2d4376e2862
LySeriuos <drungilas.p@gmail.com>
2022-04-10 20:01:55 +0000

    added structure to bag.html

--- bag/urls.py
+++ bag/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name='home')
-    ]
+    path('', views.view_bag, name='view_bag')
+]
--- bag/views.py
+++ bag/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def view_bag(request):
     """ A view to return the shoping bag page """
 
     return render(request, 'bag/bag.html')
-    
