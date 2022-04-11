2f493c00af069bb8592aa4d29d5892b4965ce2d9
LySeriuos <drungilas.p@gmail.com>
2022-04-11 10:28:30 +0000

    added add_to_bag function in views.py

--- bag/views.py
+++ bag/views.py
@@ -6,3 +6,9 @@ def view_bag(request):
     """ A view to return the shoping bag page """
 
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    """ Add quantity of the specified product to the shoping bag """
+    """ need to convert it to an integer since it'll come from the template as a string. """
+
+    quantity = int(request.POST.get('quantity'))
