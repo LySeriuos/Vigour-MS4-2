fd8c97719af3b3437a64ca363761fab3bb22e37b
LySeriuos <drungilas.p@gmail.com>
2022-04-11 10:42:33 +0000

    updated add_to_bag function with  add the item to the bag or update if it already exist

--- bag/views.py
+++ bag/views.py
@@ -7,8 +7,20 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add quantity of the specified product to the shoping bag """
     """ need to convert it to an integer since it'll come from the template as a string. """
 
     quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    # check if session exist and if doesn't create one with {}
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity 
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
