9135a4036648395558b1d5585aa188818aae4785
LySeriuos <drungilas.p@gmail.com>
2022-04-16 21:51:47 +0000

    added subtotal as a second paramter

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -6,6 +6,9 @@ register = template.Library()
 
 # the register filter decorator to register our function as a template filter
 # https://docs.djangoproject.com/en/4.0/howto/custom-template-tags/
+# typo fail: if it is only one line left before @register it will not work
+
+
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
