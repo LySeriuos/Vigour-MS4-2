c7800d10b7a1a0d718c9fa7b065e0cd8fffb7ee2
LySeriuos <drungilas.p@gmail.com>
2022-04-16 12:36:17 +0000

    added if statement to check if there was chose sizes off the prodduct

--- bag/views.py
+++ bag/views.py
@@ -16,6 +16,9 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['size']
     # check if session exist and if doesn't create one with {}
     bag = request.session.get('bag', {})
 
