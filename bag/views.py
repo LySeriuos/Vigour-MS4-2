346be4bb7a6e08de71b6c60f806bae69e53d8623
LySeriuos <drungilas.p@gmail.com>
2022-04-17 19:37:07 +0000

    updated bag views.py for messages

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from shop.models import Product
 
 # Create your views here.
-
 
 def view_bag(request):
     """ A view to return the shoping bag page """
@@ -13,7 +15,7 @@ def add_to_bag(request, item_id):
     """ Add quantity of the specified product to the shoping bag """
     # need to convert it to an integer
     # since it'll come from the template as a string.
-
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -43,6 +45,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     # print(request.session['bag'])
