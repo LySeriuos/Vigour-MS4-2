45eb3aa653ef76427984d484bd50d89abcfa299f
LySeriuos <drungilas.p@gmail.com>
2022-04-18 07:24:18 +0000

    added order line item class in the models.py

--- checkout/models.py
+++ checkout/models.py
@@ -6,9 +6,9 @@ from django.conf import settings
 
 from products.models import Product
 
-
+# payment form
 class Order(models.Model):
-    # editable=False is to create unique order number
+    # editable=False is to create unique order number, generated automatically
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -20,6 +20,21 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+    # And updating the delivery cost, order total,
+    # and grand total after customer create order insatnce.
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+# A line-item will be like an individual shopping bag item.
+# Relating to a specific order
+# Use the information they put into the payment form to create an order instance.
+# After it will iterate through the items in the shopping bag.
+# Creating an order line item for each one. Then attaching it to the order.
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True, blank=True)  # 2kg, 3kg, 4kg, 5kg, 6kg
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
