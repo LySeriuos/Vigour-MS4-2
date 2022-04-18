86521017dd0a7cec1b99a91c0a75bfe35e47b176
LySeriuos <drungilas.p@gmail.com>
2022-04-18 09:42:16 +0000

    added update functon for total cost and igf statemnet for delivery ocst

--- checkout/models.py
+++ checkout/models.py
@@ -31,6 +31,17 @@ class Order(models.Model):
         """ Generate random and unique order number ysing UUID """
         return uuid.uuid4.hex.upper()
 
+    def update_total(self):
+        """ Update grand total each time a line item is added,
+        accounting for delivery costs """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDART_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save
+
     def save(self, *args, **kwargs):
         """ Override the original save method to set the order number
         if it hasn't been set already """
