67a38d5bf6bdb740478515973f0111bfc1cddd2d
LySeriuos <drungilas.p@gmail.com>
2022-04-19 10:36:18 +0000

    integrated stripe's public key

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KqEATK3tyBr1GDCbUxKLbErhD4WUPQGgGVNuiGLHKrkUQqu2BCvjCh6APrPoItCPozL4tzuEsvmzbVFch6oE5Rx00lXqnggoQ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
