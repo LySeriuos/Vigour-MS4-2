99dd90c04befa79c5ff901799c5ee8ba5eb4561d
LySeriuos <drungilas.p@gmail.com>
2022-04-20 09:57:32 +0000

    added error handling and information about error for the card

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -27,3 +27,20 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Real tiem validation errors on the card element
+
+card.addEventListener('change', function (event){
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+        <span class="icon" role=alert>
+        <i class='fas fa-times-circle' style='font-size:22px;color:red'></i>
+        </span>
+        <span>${event.error.message}</span>
+        `
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = ''
+    }
+});
