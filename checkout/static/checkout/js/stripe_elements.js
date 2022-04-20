43b1d1bd58a4ce2d4ac6b47879d291a07e5e7aaa
LySeriuos <drungilas.p@gmail.com>
2022-04-20 12:13:47 +0000

    added accept card payment code from stripe.com and updated for to prevent multiple submissions

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,9 +6,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-let client_secret = $('#id_client_secret').text().slice(1, -1);
-let stripe = Stripe(stripe_public_key);
+let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+let clientSecret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
 let style = {
     base: {
@@ -43,4 +43,39 @@ card.addEventListener('change', function (event){
     } else {
         errorDiv.textContent = ''
     }
+});
+
+// Handle form Submit
+// https://stripe.com/docs/payments/accept-card-payments
+
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+    //  disable both the card element 
+    // and the submit button to prevent multiple submissions
+    card.update({'disabled': true});
+    $('#submit-button').attr('disabled', true);
+  // If the client secret was rendered server-side as a data-secret attribute
+  // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+     
+    }
+  }).then(function(result) {
+    if (result.error) {
+      // Show error to your customer (for example, insufficient funds)
+      console.log(result.error.message);
+    } else {
+      // The payment has been processed!
+      if (result.paymentIntent.status === 'succeeded') {
+        // Show a success message to your customer
+        // There's a risk of the customer closing the window before callback
+        // execution. Set up a webhook or plugin to listen for the
+        // payment_intent.succeeded event that handles any business critical
+        // post-payment actions.
+      }
+    }
+  });
 });
