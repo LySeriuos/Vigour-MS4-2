803a068502aad9cb1ce15173ef3705a52b6a383b
LySeriuos <drungilas.p@gmail.com>
2022-04-20 16:05:40 +0000

    added function to handel successfull checkout

--- checkout/views.py
+++ checkout/views.py
@@ -102,3 +102,13 @@ def checkout(request):
     }
 
     return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """Handle successful checkouts"""
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_namber=order_number)
+    messages.success(request, f'Ordered successfully!\
+       Your order number is {order_number}.A confirmation \
+           email will be sent to {order.email}.')
+           
