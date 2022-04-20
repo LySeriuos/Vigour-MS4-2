9c500e2fe2a54e22a962af16ffaaf3cc4fa5c18f
LySeriuos <drungilas.p@gmail.com>
2022-04-20 11:42:39 +0000

    added values from settings to public and secret keys

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,11 @@ import stripe
 
 
 def checkout(request):
+    # to set piblic and secret keys 
+    # use command 'export STRIPE_PUBLIC_KEY=...' on windows
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your bag is empty")
@@ -16,16 +21,27 @@ def checkout(request):
 
     # to get the total all I need to do is get 
     # the grand_total key out of the current bag
-    current_bag = bag_contents(reuquest)
+    current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your variables?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KqEATK3tyBr1GDCbUxKLbErhD4WUPQGgGVNuiGLHKrkUQqu2BCvjCh6APrPoItCPozL4tzuEsvmzbVFch6oE5Rx00lXqnggoQ',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
