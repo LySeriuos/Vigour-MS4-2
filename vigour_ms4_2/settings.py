4e17ceb0ecbf3e1dc1fdf518bef097b6579bc8a5
LySeriuos <drungilas.p@gmail.com>
2022-04-21 08:08:46 +0000

    removed secret key

--- vigour_ms4_2/settings.py
+++ vigour_ms4_2/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w9esh7wwv3nll9wg0sypnd#wer*r#%8z9x@s0k=*a#27j_y3qj'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['lyseriuos-vigour-ms4-2.herokuapp.com', 'localhost']
 
