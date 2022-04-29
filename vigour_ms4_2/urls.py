ed3ac9c7569b2633aec7e3687ba7165fcb9dd309
LySeriuos <drungilas.p@gmail.com>
2022-04-29 09:44:21 +0000

    created profile urls, views and template

--- vigour_ms4_2/urls.py
+++ vigour_ms4_2/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # use the static function to add the MEDIA_URL to our list of URLs.
