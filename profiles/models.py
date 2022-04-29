5ed2a0316c64b6014f913449ec2090c8353b53f1
LySeriuos <drungilas.p@gmail.com>
2022-04-29 09:03:40 +0000

    copied from checkout models

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# each user can only have one profile.
+# And each profile can only be attached to one user
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
+    user = models.OnetoOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
