53955fd3a87afb8be28d1ca2f37c321abfa660a7
LySeriuos <drungilas.p@gmail.com>
2022-04-29 09:22:07 +0000

    added function to check if user has profile, then create or update

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
@@ -20,3 +22,17 @@ class UserProfile(models.Model):
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80,True=True, blankTrue)
     default_county = models.CharField(max_length=80,True=True, blankTrue)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    isinstance.userprofile.save()    
