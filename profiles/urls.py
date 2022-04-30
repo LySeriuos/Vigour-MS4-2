366a7c175826d019f517892c95fe037b3fa6194c
LySeriuos <drungilas.p@gmail.com>
2022-04-30 17:14:46 +0000

    added function to order_history

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('order_history/<order_number>order_history', views.order_history, name='order_history'),
     ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     """Display the user's profile"""
@@ -23,6 +25,24 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is past confirmationfor order number {order_number}.'
+        'A confirmation Email was sent direct after payment'
+    ))
+
+    template = 'checkout/chekout_success.html'
+    # from_profile is to check if user got in order history by profile.html
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
