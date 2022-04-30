34e179e2d6cc036e86e3ca7ca5c040ff47baaaf0
LySeriuos <drungilas.p@gmail.com>
2022-04-30 12:18:02 +0000

    updated urls for shop app

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.shop, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    # use 'int' otherwise 'add/' can be read as product id
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
