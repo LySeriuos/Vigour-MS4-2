17f4682f16fe1272f8663811bf2e07a8700d4b09
LySeriuos <drungilas.p@gmail.com>
2022-05-21 21:36:17 +0000

    added order checking function to check if it is in database

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -25,10 +25,36 @@ class StripeWH_Handler:
         """
         Handle a handle_payment_intent_succeeded webhook from stripe
         """
-        intent = stripe.PaymentIntent.modify(event)
-        print(intent)
+        intent = event.data.object(event)
+        pid = intent.id
+        bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping
+        grand_total = round(intent.data.charges[0].amount / 100, 2)
+
+        # Shipping data details cleaning
+        for field, value in shipping_details.Address.items():
+            if value == "":
+                shipping_details.address[field] = None
+        # Check if order doesn't exists in database
+        # get the order data
+        order_exists = False
+        order = Order.objects.get(
+            full_name__iexact=shipping_details.name,
+            email_name__iexact=shipping_details.email,
+            phone_number__iexact=shipping_details.phone,
+            country__iexact=shipping_details.country,
+            postcode__iexact=shipping_details.postal_code,
+            town_or_city__iexact=shipping_details.city,
+            street_address1__iexact=shipping_details.address1,
+            street_address2__iexact=shipping_details.address2,
+            grand_total=grand_total,
+        )
+        order_exists = True        
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Webhook received: {event["type"]} | Success: Verified order already in database',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
