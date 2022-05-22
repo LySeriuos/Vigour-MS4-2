d10addd80cf6cb60b4142c458828737e2e05df99
LySeriuos <drungilas.p@gmail.com>
2022-05-22 13:08:26 +0000

    created functions to check if order exists and if not create a new one. Added order checking loop to check if order exists 5 times in 1 second

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -38,24 +38,83 @@ class StripeWH_Handler:
         for field, value in shipping_details.Address.items():
             if value == "":
                 shipping_details.address[field] = None
-        # Check if order doesn't exists in database
+        # Check if order exists in database
         # get the order data
         order_exists = False
-        order = Order.objects.get(
-            full_name__iexact=shipping_details.name,
-            email_name__iexact=shipping_details.email,
-            phone_number__iexact=shipping_details.phone,
-            country__iexact=shipping_details.country,
-            postcode__iexact=shipping_details.postal_code,
-            town_or_city__iexact=shipping_details.city,
-            street_address1__iexact=shipping_details.address1,
-            street_address2__iexact=shipping_details.address2,
-            grand_total=grand_total,
-        )
-        order_exists = True        
-        return HttpResponse(
-            content=f'Webhook received: {event["type"]} | Success: Verified order already in database',
-            status=200)
+        # here setting up loop times before it do some action
+        attempt = 1
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=shipping_details.name,
+                    email_name__iexact=shipping_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.country,
+                    postcode__iexact=shipping_details.postal_code,
+                    town_or_city__iexact=shipping_details.city,
+                    street_address1__iexact=shipping_details.address1,
+                    street_address2__iexact=shipping_details.address2,
+                    grand_total=grand_total,
+                )
+                order_exists = True
+                break
+            # check if order doesn't exists in database
+            except Order.DoesNotExist:
+                # before creating a new order time.sleep gives 1 second to loop
+                # and try to find order 5 times in 1 second before it will create a new order
+                attempt += 1
+                time.sleep(1)
+                # if order exists iåt will give a success message
+        if order_exists:            
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                status=200)
+                # and if it doesn't exists it will create new one
+        else:                    
+            try:
+                order = Order.objects.create(
+                        full_name=shipping_details.name,
+                        email_name=shipping_details.email,
+                        phone_number=shipping_details.phone,
+                        country=shipping_details.country,
+                        postcode=shipping_details.postal_code,
+                        town_or_city=shipping_details.city,
+                        street_address1=shipping_details.address1,
+                        street_address2=shipping_details.address2,
+                )
+            # get the Product ID out of the bag
+                for item_id, item_data in json.loads(bag).items():
+                    product = Product.objects.get(id=item_id)
+                    # if its value is an integer it means the item that doesn't have sizes.
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                        # if the item has sizes. It will iterate through each size 
+                        # and creates a line item accordingly.
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                 product_size=size,
+                            )
+                            order_line_item.save()
+                            except_line_item.save()
+            except Exception as e:
+                    if order: 
+                        order.delete()
+                        return HttpResponse(
+                            content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                            status=500)
+                         
+        return HttpResponse(content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+                            status=500)
+
 
     def handle_payment_intent_payment_failed(self, event):
         """
