3ae3d3cbb9cf78ab043862e291bb343e2b21081e
LySeriuos <drungilas.p@gmail.com>
2022-05-28 19:45:05 +0000

    Updated webhook handler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -29,6 +29,7 @@ class StripeWH_Handler:
         """
         intent = event.data.object(event)
         pid = intent.id
+        # metadata contains username and other info
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
@@ -40,6 +41,23 @@ class StripeWH_Handler:
         for field, value in shipping_details.Address.items():
             if value == "":
                 shipping_details.address[field] = None
+
+        # Update profile information if save_info was checked
+
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:                
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.address1
+                profile.default_street_address2 = shipping_details.address.address2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         # Check if order exists in database
         # get the order data
         order_exists = False
@@ -49,6 +67,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
+                    user_profile=profile,
                     email_name__iexact=shipping_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
