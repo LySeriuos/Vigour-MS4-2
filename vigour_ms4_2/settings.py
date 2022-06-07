e1957b66a7303e79cbb498e897e1dff578d01894
LySeriuos <drungilas.p@gmail.com>
2022-06-07 01:49:56 +0000

    sync with database

--- vigour_ms4_2/settings.py
+++ vigour_ms4_2/settings.py
@@ -142,17 +142,17 @@ WSGI_APPLICATION = 'vigour_ms4_2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # Use this before deployment!
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse('postgres://ppmjynliatzdlj:9220f437dfa0eab9ff32533db641acd56d4850fe58585d5606c27c72ede29586@ec2-52-214-23-110.eu-west-1.compute.amazonaws.com:5432/d279qi7pihjc83')
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+DATABASES = {
+    'default': dj_database_url.parse('postgres://ppmjynliatzdlj:9220f437dfa0eab9ff32533db641acd56d4850fe58585d5606c27c72ede29586@ec2-52-214-23-110.eu-west-1.compute.amazonaws.com:5432/d279qi7pihjc83')
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
